6f1853efcda41b194a5dd09da2ed571c0b058d07
Nagarjun H M <arjunahms123@gmail.com>
2024-02-07 15:02:23 +0530

    cors

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://livepolling.onrender.com", {
-  withCredentials: true,
-  path: "/socket.io",
-});
+const URL = "https://livepolling-client.onrender.com";
+
+export const socket = io(URL, { withCredentials: true, path: "/socket.io" });
 
 // function to generate random 8 character room code
 export const generateRoomId = () => {
